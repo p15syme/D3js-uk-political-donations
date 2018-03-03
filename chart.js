7d874ffd84f77e2f38851df42c3276ec695abb5e
p15syme <p15syme@ionio.gr>
2018-03-03 18:16:28 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -277,24 +277,24 @@ function moveToDonations(alpha) {
 			var centreX;
 			var centreY;
 		if (d.value <= 100000) {
-				centreY = 700;
-				centreX = 300;
-
+			centreY = 700;
+			centreX = 300;
+				
 			} else if (d.value <= 500000) {
 				centreY = 600;
 				centreX = 750;
-
+				
 			} else if (d.value <= 1000000) {
 				centreY = 500;
- 				centreX = 300;
+				centreX = 300;
 				
 			} else  if (d.value <= 5000000) {
 				centreY = 400;
 				centreX = 750;
 				
 			} else  if (d.value <= maxVal) {
-				centreY = 150;
-				centreX = 150;
+				centreY = 300;
+				centreX = 300;
 			}
 
 		d.x += (centreX - d.x) * (brake + 0.06) * alpha * 1.2;
